1880fe8522687a9fe1466bc5eef47aef8e23e1e7
Tropinone <mspt5871@protonmail.com>
2024-01-24 17:33:01 +0800

    use the CRNN model

---
+++ NeuralNetwork/CRNN/__init__.py
@@ -0,0 +1,5 @@
+from NeuralNetwork.CRNN.chr import CRNN_CHR
+from NeuralNetwork.CRNN.mel import CRNN_MEL
+from NeuralNetwork.CRNN.mfccs import CRNN_MFCC
+
+__all__ = ["CRNN_CHR", "CRNN_MEL", "CRNN_MFCC"]
